9bda012d87098c0ec815e50564ab8c537fb1db94
Christian Gerbig <christian.gerbig@pushh.it>
2023-05-05 11:44:27 +0200

    form element + gues fields added

--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { Box } from "@mantine/core";
+import { Box, Select, Button, TextInput } from "@mantine/core";
+import { isNotEmpty, useForm } from "@mantine/form";
 import { Database } from "~/lib/database.types";
-import { Select } from "@mantine/core";
 
 interface ReservationFormProps {
   shows: Database["public"]["Tables"]["shows"]["Row"][];
   reservations: Database["public"]["Tables"]["reservations"]["Row"][];
+  user: {};
 }
 
 interface ShowDateEntry {
@@ -13,13 +14,16 @@ interface ShowDateEntry {
   label: string;
 }
 
-const ReservationForm = ({ shows, reservations }: ReservationFormProps) => {
+const ReservationForm = ({
+  user,
+  shows,
+  reservations,
+}: ReservationFormProps) => {
   const [showDatesSelection, setShowDatesSelection] = useState<ShowDateEntry[]>(
     [],
   );
   const [selectedShow, setSelectedShow] = useState<string | null>(null);
-  const [freePlacesSelection, setFreePlacesSelection] = useState<string[]>([]);
-  const [selectedPlace, setSelectedPlace] = useState<string | null>(null);
+  const [freeSeatsSelection, setFreeSeatsSelection] = useState<string[]>([]);
 
   useEffect(() => {
     setShowDatesSelection(
@@ -40,37 +44,78 @@ const ReservationForm = ({ shows, reservations }: ReservationFormProps) => {
       return null;
     });
 
-    let freePlaces = [];
+    let freeSeats = [];
     for (let placeNumber = 1; placeNumber < 11; placeNumber++) {
       if (!reservedPlaceNumbers.includes(placeNumber)) {
-        freePlaces.push(placeNumber.toString());
+        freeSeats.push(placeNumber.toString());
       }
     }
-    setFreePlacesSelection(freePlaces);
+    setFreeSeatsSelection(freeSeats);
   }, [selectedShow]);
 
-  useEffect(() => {
-    console.log("Selected place: ", selectedPlace);
-  }, [selectedPlace]);
+  const form = useForm({
+    initialValues: {
+      show: "",
+      seat: "",
+      guestFirstName: "",
+      guestSurname: "",
+    },
+    validate: {
+      show: isNotEmpty(),
+      seat: isNotEmpty(),
+    },
+  });
+
+  const handleSubmit = (values: any) => {
+    form.reset();
+    console.log("Submit", values);
+  };
 
   return (
     <Box mt="4rem" mb="4rem">
-      <Select
-        data={showDatesSelection}
-        label="Show"
-        placeholder="Choose a show..."
-        onChange={setSelectedShow}
-        mb="1.5rem"
-        maw="10rem"
-      />
-
-      <Select
-        data={freePlacesSelection}
-        label="Free Places"
-        placeholder="Choose a place..."
-        onChange={setSelectedPlace}
-        maw="10rem"
-      />
+      <form
+        onSubmit={form.onSubmit((values) => {
+          handleSubmit(values);
+        })}
+      >
+        <Select
+          data={showDatesSelection}
+          label="Show"
+          placeholder="Wähle eine Show aus..."
+          withAsterisk
+          mb="1.5rem"
+          maw="10rem"
+          {...form.getInputProps("show")}
+          onChange={(values) => {
+            setSelectedShow(values);
+            values && form.setValues({ show: values });
+          }}
+        />
+        <Select
+          data={freeSeatsSelection}
+          label="Plätze"
+          placeholder="Wähle einen Platz aus..."
+          withAsterisk
+          mb="2.5rem"
+          maw="10rem"
+          {...form.getInputProps("seat")}
+        />
+        <TextInput
+          label="Gast Vorname"
+          placeholder="Vorname"
+          {...form.getInputProps("guestFirstName")}
+          mb="2.5rem"
+          maw="10rem"
+        />
+        <TextInput
+          label="Gast Nachname"
+          placeholder="NachName"
+          {...form.getInputProps("guestSurname")}
+          mb="2.5rem"
+          maw="10rem"
+        />
+        <Button type="submit">Platz buchen</Button>
+      </form>
     </Box>
   );
 };
