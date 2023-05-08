f97db339b3ee20c61b1600a8939fc12acf21b649
Dennis Kossowski <dennis.kossowski@pushh.it>
2023-05-08 09:35:17 +0200

    Update database types

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -4,81 +4,81 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       reservations: {
         Row: {
-          created_at: string | null;
-          guest_name: string | null;
-          guest_surname: string | null;
-          id: number;
-          is_guest: boolean;
-          seat: number;
-          show: number;
-          user: string;
-        };
+          created_at: string | null
+          guest_firstname: string | null
+          guest_surname: string | null
+          id: number
+          is_guest: boolean
+          seat: number
+          show: number
+          user: string
+        }
         Insert: {
-          created_at?: string | null;
-          guest_name?: string | null;
-          guest_surname?: string | null;
-          id?: number;
-          is_guest?: boolean;
-          seat: number;
-          show: number;
-          user: string;
-        };
+          created_at?: string | null
+          guest_firstname?: string | null
+          guest_surname?: string | null
+          id?: number
+          is_guest?: boolean
+          seat: number
+          show: number
+          user: string
+        }
         Update: {
-          created_at?: string | null;
-          guest_name?: string | null;
-          guest_surname?: string | null;
-          id?: number;
-          is_guest?: boolean;
-          seat?: number;
-          show?: number;
-          user?: string;
-        };
-      };
+          created_at?: string | null
+          guest_firstname?: string | null
+          guest_surname?: string | null
+          id?: number
+          is_guest?: boolean
+          seat?: number
+          show?: number
+          user?: string
+        }
+      }
       shows: {
         Row: {
-          created_at: string | null;
-          date: Date;
-          id: number;
-          movie_description: string | null;
-          movie_poster: string | null;
-          movie_title: string;
-        };
+          created_at: string | null
+          date: string
+          id: number
+          movie_description: string | null
+          movie_poster: string | null
+          movie_title: string
+        }
         Insert: {
-          created_at?: string | null;
-          date: Date;
-          id?: number;
-          movie_description?: string | null;
-          movie_poster?: string | null;
-          movie_title: string;
-        };
+          created_at?: string | null
+          date: string
+          id?: number
+          movie_description?: string | null
+          movie_poster?: string | null
+          movie_title: string
+        }
         Update: {
-          created_at?: string | null;
-          date?: Date;
-          id?: number;
-          movie_description?: string | null;
-          movie_poster?: string | null;
-          movie_title?: string;
-        };
-      };
-    };
+          created_at?: string | null
+          date?: string
+          id?: number
+          movie_description?: string | null
+          movie_poster?: string | null
+          movie_title?: string
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
