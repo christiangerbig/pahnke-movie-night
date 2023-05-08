6512be0b6d93b97551c83685c40b03fe2bfc4cc3
Christian Gerbig <christian.gerbig@pushh.it>
2023-05-08 08:21:00 +0200

    addReervation() type changed

--- src/api/addReservation.ts
+++ src/api/addReservation.ts
@@ -1,6 +1,9 @@
+import type { Database } from "~/lib/database.types";
 import { supabaseAuthClient } from "~/pages";
 
-export const addReservation = async (reservation: any) => {
+export const addReservation = async (
+  reservation: Database["public"]["Tables"]["reservations"]["Insert"],
+) => {
   const feedback = await supabaseAuthClient
     .from("reservations")
     .insert(reservation);
